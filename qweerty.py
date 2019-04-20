6545406e387973dfa0c859bf6bb6de9dab78bcd5
Eugene Orlov <>
2019-04-20 12:57:46 +0300

    y

--- qweerty.py
+++ qweerty.py
@@ -1,7 +1,24 @@
 import re
 
-with open('pog.html', encoding = 'utf-8') as f:
-    t = f.read()
-    q = re.findall('<td>(.+?)</td>', t)
-    
-    print(q)
+def strip_saying(filename):
+    with open(filename, encoding='utf-8') as f:
+        text = f.readlines()
+    for i, line in enumerate(text):
+        text[i] = line.split('\t')
+        for j, half in enumerate(text[i]):
+            text[i][j] = text[i][j].rstrip('\n ')
+    print(text)
+    with open('sayings.tsv', 'w') as f:
+        for line in text:
+            f.write('\t'.join(line) + '\n')
+
+
+
+
+
+def main():
+    strip_saying('поговорки - все вместе.tsv')
+
+
+if __name__ == '__main__':
+    main()
